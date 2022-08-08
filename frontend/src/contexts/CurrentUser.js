fcad7b2742ecee5845b1a9132723fbe3ab7a9ad9
JShinault0620 <jshincool@gmail.com>
2022-08-08 16:16:48 -0400

    Initial push

---
+++ frontend/src/contexts/CurrentUser.js
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+
+
+export const CurrentUser = createContext()
+
+function CurrentUserProvider({ children }){
+
+    const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        const getLoggedUser = async () => {
+            let res = await fetch('http://localhost:5000/authentication/profile')
+            let user = await res.json()
+            setCurrentUser(user)
+        }
+        getLoggedUser()
+    }, [])
+
+    return (
+        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+            {children}
+        </CurrentUser.Provider>
+    )
+}
+
+export default CurrentUserProvider
